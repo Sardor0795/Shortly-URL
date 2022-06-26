3a8f2544a07c8e07b848f9836861d4a4b7f5d7f2
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-06-26 08:51:24 +0500

    Add event listener

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const elJsInputForm = document.querySelector(".js-input__form");
+const elJsResult = document.querySelector(".js-result");
+const elBtnCopy = document.querySelector(".js-copy-btn");
+
+if (elJsInputForm) {
+  elJsInputForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    elJsResult.classList.add("result--open");
+  });
+}
+
+if (elJsResult) {
+  elJsResult.addEventListener("click", function (e) {
+    if (e.target.matches(".js-copy-btn")) {
+      e.target.textContent = "Copied";
+      e.target.classList.add("btn-copied");
+      setTimeout(function () {
+        e.target.textContent = "Copy";
+        e.target.classList.remove("btn-copied");
+      }, 1000);
+    }
+  });
+}
