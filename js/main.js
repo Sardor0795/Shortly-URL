da83eb0a229b2eefb8352a6fe6adf7bf4f242e04
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-04-23 11:54:19 +0500

    Add all js codes in dom content loaded

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-const elJsInputForm = document.querySelector(".js-input__form");
-const elJsResult = document.querySelector(".js-result");
-const elBtnCopy = document.querySelector(".js-copy-btn");
+window.addEventListener("DOMContentLoaded", () => {
+  const elJsInputForm = document.querySelector(".js-input__form");
+  const elJsResult = document.querySelector(".js-result");
+  const elBtnCopy = document.querySelector(".js-copy-btn");
 
-if (elJsInputForm) {
-  elJsInputForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    elJsResult.classList.add("result--open");
-  });
-}
+  if (elJsInputForm) {
+    elJsInputForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      elJsResult.classList.add("result--open");
+    });
+  }
 
-if (elJsResult) {
-  elJsResult.addEventListener("click", function (e) {
-    if (e.target.matches(".js-copy-btn")) {
-      e.target.textContent = "Copied";
-      e.target.classList.add("btn-copied");
+  if (elJsResult) {
+    elJsResult.addEventListener("click", function (e) {
+      if (e.target.matches(".js-copy-btn")) {
+        e.target.textContent = "Copied";
+        e.target.classList.add("btn-copied");
 
-      navigator.clipboard.writeText(
-        e.target.previousElementSibling.textContent
-      );
+        navigator.clipboard.writeText(
+          e.target.previousElementSibling.textContent
+        );
 
-      setTimeout(function () {
-        e.target.textContent = "Copy";
-        e.target.classList.remove("btn-copied");
-      }, 1000);
-    }
-  });
-}
+        setTimeout(function () {
+          e.target.textContent = "Copy";
+          e.target.classList.remove("btn-copied");
+        }, 1000);
+      }
+    });
+  }
+});
